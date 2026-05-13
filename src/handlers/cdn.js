ce36cd20e9f5daefa018dd9ee5bab26460548274
Narendra chavan <narendra@seattlenewmedia.com>
2026-05-14 02:45:45 +0530

    web-app sync

--- src/handlers/cdn.js
+++ src/handlers/cdn.js
@@ -744,7 +744,7 @@ function remToPx(rem, baseFontSize = 16) {
 const banerBr=remToPx(parseFloat(customization?.bannerBorderRadius) || 12);
 const loaderIab=`
 ${inlineConfig}
-${getLoaderIabScript(customization)}
+${getLoaderIabScript(customization, { rawPos: customization?.position || 'bottom-left', bannerLayoutVisual: enTrans?.bannerLayoutVisual })}
 `
 
   // loaderWebflow: standard banner behaviour with script blocking delegated to consent.js.
